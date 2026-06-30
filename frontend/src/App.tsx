12315ca391b11d5e4ab4253b70b48fd1c8f42cf1
Sebastian Andreasson <sebastian.andreasson93@gmail.com>
2026-06-30 10:50:13 +0200

    Vite, shadcn, ad-sdk stuff

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button"
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <Button>Hello world</Button>
+    </>
+  )
+}
+
+export default App
